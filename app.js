d1427fb90ce598371b3522b4abecf32fc6ff09f4
matterwin <matthewderwin23@gmail.com>
2023-05-17 16:19:05 -0500

    on event loops

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 // or 
 // npm --version
 
-// local dependecy - use it in any project
+// local dependecy - use it only in this particular project
+// npm i <packageName>
+
+// global dependency - use it in any project
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
 
@@ -17,3 +20,8 @@ const _ = require('lodash')
 const items = [1,[2,[3, [4]]]]
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log("hello people")
+
+//nodemon simply keeps updating server continuously without
+//having us to manually restart
+//prof suggests to use something more serious like mpm2 or something
