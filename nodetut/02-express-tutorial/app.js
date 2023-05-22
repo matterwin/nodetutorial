921668e96abf87bda0ce48b435312c43a22e5f52
Matthew Erwin <matthewderwin23@gmail.com>
2023-05-22 00:57:57 -0500

    done with tut

--- nodetut/02-express-tutorial/app.js
+++ nodetut/02-express-tutorial/app.js
@@ -1,58 +1,28 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+const express = require('express');
+const app = express();
+let { people } = require('./data')
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products', (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
+//static assests
+app.use(express.static('./methods-public'))
 
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  console.log(req.params)
-  const { productID } = req.params
+//parse form data
+app.use(express.urlencoded({extended:false}))
 
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  )
-  if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
-  }
+//parse json
+app.use(express.json());
 
-  return res.json(singleProduct)
+app.get('/api/people', (req,res)=>{
+    res.status(200).json({ success: true, person: name })
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
+app.post('/api/people',(req,res)=>{
+    const {name}=req.body;
+    if(!name){
+        return res.status(400).json({success:false,msg:'pls provide name value'})        
+    }
+    res.status(200).json('Please Provide Credentials')
 })
 
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
-  }
-  res.status(200).json(sortedProducts)
-})
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+app.listen(5000,()=>{
+    console.log("connected")
 })
