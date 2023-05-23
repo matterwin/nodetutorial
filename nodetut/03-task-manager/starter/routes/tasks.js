b06211db505ecdcb5eb830584f579dfe36ee954e
Matthew Erwin <matthewderwin23@gmail.com>
2023-05-22 19:38:57 -0500

    node

---
+++ nodetut/03-task-manager/starter/routes/tasks.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router();
+
+const {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/tasks')
+
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).post(updateTask).delete(deleteTask);
+
+
+// app.get('/api/v1/tasks')             - get all the tasks
+// app.post('/api/v1/tasks')            - create a new task
+// app.get('/api/v1/tasks/:id')         - get single task
+// app.patch('/api/v1/tasks/:id')       - update task
+// app.delete('/api/v1/tasks/:id')      - delete task
+
+module.exports = router
