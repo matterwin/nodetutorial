da957f7e65d1c4723033055836aba368f10e4c7c
Matthew Erwin <matthewderwin23@gmail.com>
2023-05-25 00:42:51 -0500

    done with task proj

--- nodetut/03-task-manager/starter/app.js
+++ nodetut/03-task-manager/starter/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
-const port = 3000;
 const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -15,6 +14,8 @@ app.use(express.json())
 app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
