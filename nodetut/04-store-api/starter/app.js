5e17c0173a989792a3d8f3563014a558b91acdd8
Matthew Erwin <matthewderwin23@gmail.com>
2023-05-25 12:40:24 -0500

    prog4

--- nodetut/04-store-api/starter/app.js
+++ nodetut/04-store-api/starter/app.js
@@ -1,1 +1,40 @@
-console.log('04 Store API')
+require('dotenv').config()
+const connectDB = require('./db/connect')
+
+//async errors
+
+const express = require('express')
+const app = express()
+
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+//middleware
+app.use(express.json())
+
+//routes
+
+app.get('/', (req,res)=> {
+    res.send(`<h1> Store API</h1><a href="/api/vi/products">products route</a>`)
+})
+
+//products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        //connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log("Server on port"))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
