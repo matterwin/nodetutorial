a1b0c27b89363250167c8c3cb53894b1d15afb6d
Matthew Erwin <matthewderwin23@gmail.com>
2023-05-25 00:37:00 -0500

    error handling

--- nodetut/03-task-manager/starter/app.js
+++ nodetut/03-task-manager/starter/app.js
@@ -4,6 +4,7 @@ const tasks = require('./routes/tasks')
 const port = 3000;
 const connectDB = require('./db/connect')
 const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 require('dotenv').config()
 
 // middleware
@@ -13,6 +14,7 @@ app.use(express.json())
 //routes
 app.use('/api/v1/tasks', tasks)
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async () => {
     try {
