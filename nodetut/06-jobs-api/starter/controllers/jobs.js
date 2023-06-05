0d736a73f6abb2abcc9492d9e9b7dae6ae5cbf0e
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-04 19:05:56 -0500

    done with routes

--- nodetut/06-jobs-api/starter/controllers/jobs.js
+++ nodetut/06-jobs-api/starter/controllers/jobs.js
@@ -1,22 +1,74 @@
+const {StatusCodes} = require('http-status-codes')
+const Job = require('../models/Job')
+const {BadRequestError, NotFoundError} = require('../errors')
 
 const getAllJobs = async (req,res) => {
-    res.send('getAllJobs')
+    const jobs = await Job.find({createdBy:req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({count:jobs.length, jobs })
 }
 
 const getJob = async (req,res) => {
-    res.send('getJob')
+    const {user:{userId}, params:{id:jobId}} = req
+
+    const job = await Job.findOne({
+        _id:jobId,
+        createdBy:userId
+    })
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req,res) => {
-    res.json(req.user)
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req,res) => {
-    res.send('updateJob')
+    const {
+        body:{company,position},
+        user:{userId}, 
+        params:{id:jobId}
+    } = req
+
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+
+    const job = await Job.findOneAndUpdate(
+        {
+            _id:jobId,
+            createdBy:userId
+        },
+        req.body,
+        {
+            new:true,
+            runValidators:true
+        }
+    )
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob = async (req,res) => {
-    res.send('deleteJob')
+    const {
+        user:{userId}, 
+        params:{id:jobId}
+    } = req
+
+    const job = await Job.findOneAndRemove({
+        _id:jobId,
+        createdBy:userId
+    })
+
+    if(!job){
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send('Deleted Successfully')
 }
 
 module.exports = {
