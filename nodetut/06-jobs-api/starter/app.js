785e9d8ed74552c8d502c4be157aa82152e81a3c
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-04 19:40:49 -0500

    adding security libs

--- nodetut/06-jobs-api/starter/app.js
+++ nodetut/06-jobs-api/starter/app.js
@@ -3,11 +3,18 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+
 // connectDB
 const connectDB = require('./db/connect')
 const authenticateUser = require('./middleware/authentication')
 
-//routers
+// routers
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 
@@ -15,8 +22,18 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
-// extra packages
+app.set('trust proxy',1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 mins
+    max: 100, // limit each IP to 100 requests per windowMw
+  })
+)
+app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // routes
 app.use('/api/v1/auth', authRouter)
