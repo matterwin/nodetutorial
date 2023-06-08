86492389941fa8b8c9305b09a5f82d69a3fbec58
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-07 23:08:39 -0500

    done with image upload

--- nodetut/07-file-upload/starter/controllers/uploadsController.js
+++ nodetut/07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,26 @@
 const path = require('path')
 const {StatusCodes} = require('http-status-codes')
 const CustomError = require('../errors')
+const cloudinary = require('cloudinary').v2
+const fs = require('fs')
 
-const uploadProductImage = async(req, res) => {
+const uploadProductImage = async(req,res) => {
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,
+        {
+            use_filename:true,
+            folder:'file-upload'
+        }
+    )
+    fs.unlinkSync(req.files.image.tempFilePath);
+    return res
+        .status(StatusCodes.OK)
+        .json({
+            image:{src:result.secure_url}
+        })
+}
+
+const uploadProductImageLocal = async(req, res) => {
     if(!req.files){
         throw new CustomError.BadRequestError('No File Uploaded')
     }
