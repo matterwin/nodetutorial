803325a735c47dd35f35d8b7807e07934377f093
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-07 22:50:14 -0500

    profile-pic-upload

---
+++ nodetut/07-file-upload/starter/controllers/uploadsController.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+
+const uploadProductImage = async(req, res) => {
+    if(!req.files){
+        throw new CustomError.BadRequestError('No File Uploaded')
+    }
+    const productImage = req.files.image;
+    if(!productImage.mimetype.startsWith('image')){
+        throw new CustomError.BadRequestError('Please Upload Image');
+    }
+
+    const maxSize = 1024 * 1024
+    if(productImage.size > maxSize){
+        throw new CustomError.BadRequestError('Please Upload Image smaller than 1KB');
+    }
+
+    const imagePath = path.join(__dirname,'../public/uploads/' +`${productImage.name}`)
+
+    await productImage.mv(imagePath)
+    return res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}})
+}
+
+module.exports = {
+    uploadProductImage
+}
