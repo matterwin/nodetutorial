d974b70ef6531cefe4230a0593c4bf1c3468fd9e
Matthew Erwin <matthewderwin23@gmail.com>
2023-06-09 23:16:47 -0500

    auth work

---
+++ nodetut/10-e-commerce-api/starter/controllers/userController.js
@@ -0,0 +1,49 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+
+const getAllUsers = async(req,res) => {
+    const users = await User.find({role:'user'}).select('-password')
+    res.status(StatusCodes.OK).json({users});
+}
+
+const getSingleUser = async(req,res) => {
+    const user = await User.findOne({_id:req.params.id}).select('-password')
+    if(!user){
+        throw new CustomError.NotFoundError(`No user with id ${req.params.id}`)
+    }
+    res.status(StatusCodes.OK).json({user});
+}
+
+const showCurrentUser = async(req,res) => {
+    res.status(StatusCodes.OK).json({user:req.user})
+}
+
+const updateUser = async(req,res) => {
+    res.send('updateUser')
+}
+
+const updateUserPassword = async(req,res) => {
+    const {oldPassword, newPassword} = req.body
+    if (!oldPassword || !newPassword) {
+        throw new CustomError.BadRequestError('Please provide both values');
+    }
+    const user = await User.findOne({_id:req.user.userId})
+
+    const isPasswordCorrect = await user.comparePassword(oldPassword)
+    if(!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid creds')
+    }
+    user.password = newPassword;
+
+    await user.save();
+    res.status(StatusCodes.OK).json({msg:'Success! Password updated'})
+}
+
+module.exports = {
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
+    updateUserPassword
+}
